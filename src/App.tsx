7139f58ab9b96ff53e68da0ac27c130e713a1e1c
snehomoy100@gmail.com <snehomoy100@gmail.com>
2022-06-25 19:23:45 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
+import FileUpload from "react-material-file-upload";
 
-function App() {
+export default function App() {
+  const [files, setFiles] = useState<File[]>([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Paper.Do Frontend</h1>
+      <FileUpload value={files} onChange={setFiles} />
     </div>
   );
 }
-
-export default App;
